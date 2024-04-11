cf07289b5bd5c497768a4b43e289850c3acf1f1d
Rithik Roshan <116257183+rithik2410@users.noreply.github.com>
2024-04-11 22:14:45 +0530

    Add files via upload

---
+++ Voting_Application_using_Django_Framework-RithikRoshan(4033,SEC)/vote/Pollingproject/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('signin/', views.signin, name ='signin'),
+    path('login/', views.Home, name ='login'),
+	path('index/', views.index, name ='index'),
+	path('<int:question_id>/', views.detail, name ='detail'),
+	path('<int:question_id>/results/', views.results, name ='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
+]
